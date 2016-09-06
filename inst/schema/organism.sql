5c984982198c4fdf63a53c37286f350ca6aaaf4d
Martin Morgan <martin.morgan@roswellpark.org>
2016-09-06 11:35:19 -0400

    updated schema
    - separate genes, tx, exon, cds of txdb as ranges_*
    - rename organism tables id_*

--- inst/schema/organism.sql
+++ inst/schema/organism.sql
@@ -1,15 +1,15 @@
-CREATE TEMPORARY VIEW IF NOT EXISTS accession AS 
+CREATE TEMPORARY VIEW IF NOT EXISTS id_accession AS
 SELECT DISTINCT
-    genes.gene_id AS entrez, 
-    accessions.accession AS accnum, 
+    genes.gene_id AS entrez,
+    accessions.accession AS accnum,
     refseq.accession AS refseq
-FROM genes 
+FROM genes
 JOIN gene_info ON genes._id = gene_info._id
 LEFT OUTER JOIN accessions ON genes._id = accessions._id
 LEFT OUTER JOIN refseq ON genes._id = refseq._id
     AND refseq.accession = accessions.accession;
 
-CREATE TEMPORARY VIEW IF NOT EXISTS transcript AS
+CREATE TEMPORARY VIEW IF NOT EXISTS id_transcript AS
 SELECT DISTINCT
     genes.gene_id AS entrez,
     unigene.unigene_id AS unigene,
@@ -18,21 +18,21 @@ FROM genes
 LEFT OUTER JOIN unigene ON genes._id = unigene._id
 LEFT OUTER JOIN ensembl_trans ON genes._id = ensembl_trans._id;
 
-CREATE TEMPORARY VIEW IF NOT EXISTS id AS 
+CREATE TEMPORARY VIEW IF NOT EXISTS id AS
 SELECT DISTINCT
     genes.gene_id AS entrez,
-    cytogenetic_location AS map, 
-    ensembl.ensembl_id AS ensembl, 
+    cytogenetic_location AS map,
+    ensembl.ensembl_id AS ensembl,
     gene_info.symbol AS symbol,
-    gene_info.gene_name AS genename, 
+    gene_info.gene_name AS genename,
     alias.alias_symbol AS alias
-FROM genes 
+FROM genes
 JOIN gene_info ON genes._id = gene_info._id
 JOIN cytogenetic_locations ON genes._id = cytogenetic_locations._id
 JOIN ensembl ON genes._id = ensembl._id
 LEFT OUTER JOIN alias ON genes._id = alias._id;
 
-CREATE TEMPORARY VIEW IF NOT EXISTS omim_pmid AS
+CREATE TEMPORARY VIEW IF NOT EXISTS id_omim_pm AS
 SELECT DISTINCT
     genes.gene_id AS entrez,
     omim.omim_id AS omim,
@@ -41,7 +41,7 @@ FROM genes
 JOIN omim ON genes._id = omim._id
 LEFT OUTER JOIN pubmed ON genes._id = pubmed._id;
 
-CREATE TEMPORARY VIEW IF NOT EXISTS protein AS
+CREATE TEMPORARY VIEW IF NOT EXISTS id_protein AS
 SELECT DISTINCT
     genes.gene_id AS entrez,
     ec.ec_number AS enzyme,
@@ -58,20 +58,20 @@ LEFT OUTER JOIN pfam ON genes._id = pfam._id
 LEFT OUTER JOIN prosite ON genes._id = prosite._id
     AND pfam.ipi_id = prosite.ipi_id;
 
-CREATE TEMPORARY VIEW IF NOT EXISTS view_go_all AS
+CREATE TEMPORARY VIEW IF NOT EXISTS id_go AS
 SELECT DISTINCT
-    genes.gene_id AS entrez,
-    go_all.go_id AS goall,
-    go_all.evidence AS evidenceall,
-    go_all.ontology AS ontologyall
-FROM genes
-LEFT OUTER JOIN go_all ON genes._id = go_all._id;
+    main.genes.gene_id AS entrez,
+    main.go.go_id AS go,
+    main.go.evidence AS evidence,
+    main.go.ontology AS ontology
+FROM main.genes
+LEFT OUTER JOIN go ON main.genes._id = main.go._id;
 
-CREATE TEMPORARY VIEW IF NOT EXISTS view_go AS
+CREATE TEMPORARY VIEW IF NOT EXISTS id_go_all AS
 SELECT DISTINCT
-    genes.gene_id AS entrez,
-    go.go_id AS go,
-    go.evidence AS evidence,
-    go.ontology AS ontology
-FROM genes
-LEFT OUTER JOIN go ON genes._id = go._id;
+    main.genes.gene_id AS entrez,
+    main.go_all.go_id AS goall,
+    main.go_all.evidence AS evidenceall,
+    main.go_all.ontology AS ontologyall
+FROM main.genes
+LEFT OUTER JOIN main.go_all ON main.genes._id = main.go_all._id;
--- inst/schema/txdb.sql
+++ inst/schema/txdb.sql
@@ -1,26 +1,64 @@
-CREATE TEMPORARY VIEW IF NOT EXISTS txdb AS 
+CREATE TEMPORARY VIEW IF NOT EXISTS ranges_gene AS
+SELECT DISTINCT
+    gene.gene_id AS entrez,
+    tx_chrom AS chrom,
+    MIN(tx_start) AS start,
+    MAX(tx_end) AS end,
+    tx_strand AS strand
+FROM txdb.gene
+LEFT OUTER JOIN txdb.transcript ON txdb.transcript._tx_id = txdb.gene._tx_id
+GROUP BY txdb.gene.gene_id;
+
+CREATE TEMPORARY VIEW IF NOT EXISTS ranges_tx AS
+SELECT DISTINCT
+    gene.gene_id AS entrez,
+    transcript._tx_id AS id,
+    transcript.tx_chrom AS chrom,
+    transcript.tx_strand AS strand,
+    transcript.tx_start AS start,
+    transcript.tx_end AS end
+FROM txdb.gene
+LEFT OUTER JOIN txdb.transcript ON txdb.transcript._tx_id = txdb.gene._tx_id;
+
+CREATE TEMPORARY VIEW IF NOT EXISTS ranges_exon AS
 SELECT DISTINCT
     gene.gene_id AS entrez,
     transcript._tx_id AS txid,
-    transcript.tx_name AS txname, 
-    transcript.tx_type AS txtype, 
-    transcript.tx_chrom AS txchrom, 
-    transcript.tx_strand AS txstrand, 
-    transcript.tx_start AS txstart, 
-    transcript.tx_end AS txend, 
     exon._exon_id AS exonid,
-    exon.exon_name AS exonname, 
-    exon.exon_chrom AS exonchrom, 
-    exon.exon_strand AS exonstrand, 
-    exon.exon_start AS exonstart, 
-    exon.exon_end AS exonend, 
-    splicing.exon_rank AS exonrank, 
-    cds._cds_id AS cdsid, 
-    cds.cds_name AS cdsname, 
-    cds.cds_chrom AS cdschrom, 
-    cds.cds_strand AS cdsstrand, 
-    cds.cds_start AS cdsstart, 
+    exon.exon_chrom AS exonchrom,
+    exon.exon_strand AS exonstrand,
+    exon.exon_start AS exonstart,
+    exon.exon_end AS exonend,
+    splicing.exon_rank AS exonrank
+FROM txdb.gene
+LEFT OUTER JOIN txdb.transcript ON txdb.transcript._tx_id = txdb.gene._tx_id
+LEFT OUTER JOIN txdb.splicing ON txdb.splicing._tx_id = txdb.gene._tx_id
+LEFT OUTER JOIN txdb.exon ON txdb.exon._exon_id = txdb.splicing._exon_id;
+
+CREATE TEMPORARY VIEW IF NOT EXISTS ranges_cds AS
+SELECT DISTINCT
+    gene.gene_id AS entrez,
+    transcript._tx_id AS txid,
+    cds._cds_id AS cdsid,
+    cds.cds_chrom AS cdschrom,
+    cds.cds_strand AS cdsstrand,
+    cds.cds_start AS cdsstart,
     cds.cds_end AS cdsend
+FROM txdb.gene
+LEFT OUTER JOIN txdb.transcript ON txdb.transcript._tx_id = txdb.gene._tx_id
+LEFT OUTER JOIN txdb.splicing ON txdb.splicing._tx_id = txdb.gene._tx_id
+LEFT OUTER JOIN txdb.cds ON txdb.cds._cds_id = txdb.splicing._cds_id;
+
+CREATE TEMPORARY VIEW IF NOT EXISTS id_ranges AS
+SELECT DISTINCT
+    gene.gene_id AS entrez,
+    transcript._tx_id AS txid,
+    transcript.tx_name AS txname,
+    transcript.tx_type AS txtype,
+    exon._exon_id AS exonid,
+    exon.exon_name AS exonname,
+    cds._cds_id AS cdsid,
+    cds.cds_name AS cdsname
 FROM txdb.gene
 LEFT OUTER JOIN txdb.transcript ON txdb.transcript._tx_id = txdb.gene._tx_id
 LEFT OUTER JOIN txdb.splicing ON txdb.splicing._tx_id = txdb.gene._tx_id
