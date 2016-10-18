8a4a190720eda969c618187f7e97d120a87afdbd
yubocheng <yubo.cheng@roswellpark.org>
2016-10-18 08:33:22 -0400

    add functions fiveUTRsByTranscript() and threeUTRsByTranscript()

--- R/extractors.R
+++ R/extractors.R
@@ -50,10 +50,10 @@
 
 setMethod("transcripts", "src_organism", function(x, filter = NULL) {
     table <- tbl(x, "ranges_tx")
-    table <- .tbl_join(x, table, filter)
+    table <- .tbl_join(x, table, filter) %>% arrange(tx_id)
     fields <- unique(
-        c("tx_id", "entrez", "tx_chrom", "tx_start", "tx_end", "tx_strand", 
-          "tx_name", names(filter)))
+        c("tx_chrom", "tx_start", "tx_end", "tx_strand", 
+          "entrez", "tx_id", "tx_name", names(filter)))
     do.call(select_, c(list(table), as.list(fields)))
 })
 
@@ -63,7 +63,7 @@ setMethod("transcripts", "src_organism", function(x, filter = NULL) {
 
 setMethod("exons", "src_organism", function(x, filter = NULL) {
     table <- tbl(x, "ranges_exon")
-    table <- .tbl_join(x, table, filter)
+    table <- .tbl_join(x, table, filter) %>% arrange(exon_id)
     fields <- unique(
         c("exon_chrom", "exon_start", "exon_end", "exon_strand",
           "entrez", "tx_id", "exon_id", "exon_name", "exon_rank",
@@ -78,10 +78,10 @@ setMethod("exons", "src_organism", function(x, filter = NULL) {
 
 setMethod("cds", "src_organism", function(x, filter = NULL) {
     table <- tbl(x, "ranges_cds")
-    table <- .tbl_join(x, table, filter)
+    table <- .tbl_join(x, table, filter) %>% arrange(cds_id)
     fields <- unique(
-        c("cds_id", "tx_id", "entrez", "cds_chrom", "cds_start", "cds_end", 
-          "cds_strand", "cds_name", "exon_rank", names(filter)))
+        c("cds_chrom", "cds_start", "cds_end", "cds_strand", 
+          "entrez", "tx_id", "cds_id", "cds_name", "exon_rank", names(filter)))
     do.call(select_, c(list(table), as.list(fields)))
 })
 
@@ -92,10 +92,10 @@ setMethod("cds", "src_organism", function(x, filter = NULL) {
 
 setMethod("genes", "src_organism", function(x, filter = NULL) {
     table <- tbl(x, "ranges_gene")
-    table <- .tbl_join(x, table, filter)
+    table <- .tbl_join(x, table, filter) %>% arrange(entrez)
     fields <- unique(
-        c("entrez", "tx_chrom", "gene_start", "gene_end", "tx_strand", 
-          names(filter)))
+        c("tx_chrom", "gene_start", "gene_end", "tx_strand", 
+          "entrez", names(filter)))
     do.call(select_, c(list(table), as.list(fields)))
 })
 
@@ -140,11 +140,12 @@ function(x, upstream, downstream, filter = NULL) {
                               tx_start - upstream),
             end = ifelse(tx_strand == "-", 
                          tx_end + upstream, 
-                         tx_start + downstream - 1))
+                         tx_start + downstream - 1)) %>%
+        arrange(tx_id)
     
     fields <- unique(
-        c("tx_id", "entrez", "tx_chrom", "start", "end", "tx_strand", 
-          "tx_name", names(filter)))
+        c("tx_chrom", "start", "end", "tx_strand", 
+          "entrez", "tx_id", "tx_name", names(filter)))
     do.call(select_, c(list(table), as.list(fields)))
 })
 
@@ -167,22 +168,24 @@ function(x, by = c("gene", "exon", "cds"), filter = NULL) {
     if (by == "gene") {
         table <- inner_join(tx, tbl(x, "ranges_gene")) 
         fields <- unique(
-            c("entrez", "tx_id", "tx_chrom", "tx_start", "tx_end",  
-              "tx_strand", "tx_name", names(filter)))
+            c("tx_chrom", "tx_start", "tx_end", "tx_strand",  
+              "entrez", "tx_id", "tx_name", names(filter)))
+        do.call(select_, c(list(table), as.list(fields))) %>% arrange(entrez)
     }
     else if (by == "exon") {
         table <- inner_join(tx, tbl(x, "ranges_exon"), by = "tx_id") 
         fields <- unique(
-            c("exon_id", "tx_id", "tx_chrom", "tx_start", "tx_end",  
-              "tx_strand", "tx_name", "exon_rank", names(filter)))
+            c("tx_chrom", "tx_start", "tx_end", "tx_strand",  
+              "exon_id", "tx_id", "tx_name", "exon_rank", names(filter)))
+        do.call(select_, c(list(table), as.list(fields))) %>% arrange(exon_id)
     }
     else if (by == "cds") {
         table <- inner_join(tx, tbl(x, "ranges_cds"), by = "tx_id") 
         fields <- unique(
-            c("cds_id", "tx_id", "tx_chrom", "tx_start", "tx_end",  
-              "tx_strand", "tx_name", "exon_rank", names(filter)))
+            c("tx_chrom", "tx_start", "tx_end", "tx_strand",  
+              "cds_id", "tx_id", "tx_name", "exon_rank", names(filter)))
+        do.call(select_, c(list(table), as.list(fields))) %>% arrange(cds_id)
     }
-    do.call(select_, c(list(table), as.list(fields)))
 })
 
 
@@ -201,16 +204,17 @@ function(x, by = c("tx", "gene"), filter = NULL) {
     if (by == "tx") {
         table <- inner_join(exons, tbl(x, "ranges_tx"), by = "tx_id") 
         fields <- unique(
-            c("tx_id", "exon_id", "exon_chrom", "exon_start", "exon_end",  
-              "exon_strand", "exon_name", "exon_rank", names(filter)))
+            c("exon_chrom", "exon_start", "exon_end", "exon_strand",  
+              "tx_id", "exon_id", "exon_name", "exon_rank", names(filter)))
+        do.call(select_, c(list(table), as.list(fields))) %>% arrange(tx_id)
     }
     else if (by == "gene") {
-        table <- inner_join(exons, tbl(x, "ranges_gene"))
+        table <- inner_join(exons, tbl(x, "ranges_gene")) 
         fields <- unique(
-            c("entrez", "exon_id", "exon_chrom", "exon_start", "exon_end",  
-              "exon_strand", "exon_name", names(filter)))
+            c("exon_chrom", "exon_start", "exon_end", "exon_strand",  
+              "entrez", "exon_id", "exon_name", names(filter)))
+        do.call(select_, c(list(table), as.list(fields))) %>% arrange(entrez)
     }
-    do.call(select_, c(list(table), as.list(fields)))
 })
 
 
@@ -229,36 +233,118 @@ function(x, by = c("tx", "gene"), filter = NULL) {
     if (by == "tx") {
         table <- inner_join(cds, tbl(x, "ranges_tx"), by = "tx_id") 
         fields <- unique(
-            c("tx_id", "cds_id", "cds_chrom", "cds_start", "cds_end",  
-              "cds_strand", "cds_name", "exon_rank", names(filter)))
+            c("cds_chrom", "cds_start", "cds_end", "cds_strand",  
+              "tx_id", "cds_id", "cds_name", "exon_rank", names(filter)))
+        do.call(select_, c(list(table), as.list(fields))) %>% arrange(tx_id) 
     }
     else if (by == "gene") {
-        table <- inner_join(cds, tbl(x, "ranges_gene"))
+        table <- inner_join(cds, tbl(x, "ranges_gene")) 
         fields <- unique(
-            c("entrez", "cds_id", "cds_chrom", "cds_start", "cds_end", 
-              "cds_strand", "cds_name", names(filter)))
+            c("cds_chrom", "cds_start", "cds_end", "cds_strand", 
+              "entrez", "cds_id", "cds_name", names(filter)))
+        do.call(select_, c(list(table), as.list(fields))) %>% arrange(entrez)
     }
-    do.call(select_, c(list(table), as.list(fields)))
 })
 
 
+#' @examples
+#' intronsByTranscript(organism, filter = list(symbol="PTEN"))
+#'
+#' @rdname src_organism
+#' @importFrom GenomicFeatures intronsByTranscript
+#' @importFrom GenomicRanges split
+#' @importFrom IRanges psetdiff
+#' @export
 setMethod("intronsByTranscript", "src_organism",
 function(x, filter=NULL) {
     tx <- transcripts(x, filter=filter)
     exn <- exonsBy(x, filter=filter)
 
-    tx_gr <- tx %>% select(tx_id, tx_chrom, tx_start, tx_end, tx_strand) %>%
+    tx_gr <- tx %>% 
+        dplyr::select(tx_id, tx_chrom, tx_start, tx_end, tx_strand) %>%
         collect(n=Inf) %>% as("GRanges")
-    exn_grl <- exn %>%
-        select(tx_id, exon_id, exon_chrom, exon_start, exon_end,
+    exn_grl <- exn %>% collect(n=Inf) %>% 
+        dplyr::select(tx_id, exon_id, exon_chrom, exon_start, exon_end,
                exon_strand) %>%
-        collect(n=Inf) %>% as("GRanges") %>% split(.$tx_id)
+        as("GRanges") %>% split(.$tx_id)
     
     tx_gr<- tx_gr[match(names(exn_grl), mcols(tx_gr)[, "tx_id"])]
     ans <- unlist(psetdiff(tx_gr, exn_grl))
 
     mcols(ans)[, "tx_id"] <- names(ans)
     unname(ans) %>% as.data.frame %>% tbl_df %>%
-        select(tx_id, intron_chrom=seqnames, intron_start=start,
+        dplyr::select(tx_id, intron_chrom=seqnames, intron_start=start,
                intron_end=end, intron_strand=strand)
+})
+
+
+.getSplicings <- function(x, filter=NULL) {
+    exon <- exons(x, filter=filter)
+    cds <- cds(x, filter=filter)
+    
+    inner_join(exon, cds, by = c("tx_id", "exon_rank")) %>%
+        dplyr::select(tx_id, exon_rank, exon_id, exon_name, exon_chrom, 
+                      exon_strand, exon_start, exon_end, cds_id, cds_start,  
+                      cds_end) %>% 
+        collect(n=Inf) %>%
+        arrange(tx_id, exon_rank)
+}
+
+#' @examples
+#' fiveUTRsByTranscript(organism, filter = list(symbol="PTEN"))
+#' 
+#' @rdname src_organism
+#' @importFrom GenomicFeatures fiveUTRsByTranscript
+#' @export
+setMethod("fiveUTRsByTranscript", "src_organism",function(x, filter=NULL) {
+    splicings <- .getSplicings(x, filter)
+    
+    exons_with_cds <- which(!is.na(splicings$cds_id))
+    idx <- GenomicFeatures:::.exons_with_5utr(splicings$tx_id, exons_with_cds)
+    splicings <- S4Vectors:::extract_data_frame_rows(splicings, idx)
+    
+    class(splicings) <- c("tbl_df", class(splicings))
+    
+    splicings %>% 
+        mutate(start = 
+                   ifelse(!is.na(cds_id) & exon_strand == "-",
+                        cds_end + 1L, 
+                        exon_start), 
+               end = 
+                   ifelse(!is.na(cds_id) & exon_strand == "+", 
+                        cds_start - 1L, 
+                        exon_end)) %>% 
+        filter(start <= end) %>%
+        dplyr::select(exon_chrom, start, end, exon_strand, 
+                      tx_id, exon_id, exon_name, exon_rank)
+})
+
+
+#' @examples
+#' threeUTRsByTranscript(organism, filter = list(symbol="PTEN"))
+#' 
+#' @rdname src_organism
+#' @importFrom GenomicFeatures threeUTRsByTranscript
+#' @export
+setMethod("threeUTRsByTranscript", "src_organism",function(x, filter=NULL) {
+    splicings <- .getSplicings(x, filter)
+    
+    exons_with_cds <- which(!is.na(splicings$cds_id))
+    idx <- GenomicFeatures:::.exons_with_3utr(splicings$tx_id, exons_with_cds)
+    splicings <- S4Vectors:::extract_data_frame_rows(splicings, idx)
+    
+    class(splicings) <- c("tbl_df", class(splicings))
+    
+    splicings %>% 
+        mutate(start = 
+                   ifelse(!is.na(cds_id) & exon_strand == "+",
+                          cds_end + 1L, 
+                          exon_start), 
+               end = 
+                   ifelse(!is.na(cds_id) & exon_strand == "-", 
+                          cds_start - 1L, 
+                          exon_end)) %>% 
+        filter(start <= end) %>%
+        dplyr::select(exon_chrom, start, end, exon_strand, 
+                      tx_id, exon_id, exon_name, exon_rank)
 })
--- inst/schema/txdb_entrez.sql
+++ inst/schema/txdb_entrez.sql
@@ -1,10 +1,10 @@
 CREATE TEMPORARY TABLE IF NOT EXISTS ranges_gene AS
 SELECT DISTINCT
-    gene.gene_id AS entrez,
     tx_chrom,
     MIN(tx_start) AS gene_start,
     MAX(tx_end) AS gene_end,
-    tx_strand
+    tx_strand,
+    gene.gene_id AS entrez
 FROM txdb_entrez.gene
 LEFT OUTER JOIN txdb_entrez.transcript ON transcript._tx_id = gene._tx_id
 GROUP BY entrez, tx_chrom, tx_strand;
@@ -13,12 +13,12 @@ CREATE INDEX IF NOT EXISTS entrez_ranges_gene on ranges_gene (entrez);
 
 CREATE TEMPORARY TABLE IF NOT EXISTS ranges_tx AS
 SELECT DISTINCT
-    gene.gene_id AS entrez,
-    transcript._tx_id AS tx_id,
     tx_chrom,
     tx_strand,
     tx_start,
     tx_end,
+    gene.gene_id AS entrez,
+    transcript._tx_id AS tx_id,
     tx_name,
     tx_type
 FROM txdb_entrez.transcript
@@ -35,12 +35,12 @@ SELECT DISTINCT
     exon_start,
     exon_end,
     gene.gene_id AS entrez,
-    transcript._tx_id AS tx_id,
+    splicing._tx_id AS tx_id,
     exon._exon_id AS exon_id,
     exon_name,
     exon_rank
-FROM txdb_entrez.exon
-LEFT OUTER JOIN txdb_entrez.splicing ON exon._exon_id = splicing._exon_id
+FROM txdb_entrez.splicing
+LEFT OUTER JOIN txdb_entrez.exon ON exon._exon_id = splicing._exon_id
 LEFT OUTER JOIN txdb_entrez.transcript ON splicing._tx_id = transcript._tx_id
 LEFT OUTER JOIN txdb_entrez.gene ON transcript._tx_id = gene._tx_id;
 
@@ -52,19 +52,22 @@ CREATE INDEX IF NOT EXISTS exonid_ranges_exon on ranges_exon (exon_id);
 
 CREATE TEMPORARY TABLE IF NOT EXISTS ranges_cds AS
 SELECT DISTINCT
-    gene.gene_id AS entrez,
-    transcript._tx_id AS tx_id,
-    cds._cds_id AS cds_id,
     cds_chrom,
     cds_strand,
     cds_start,
     cds_end,
+    gene.gene_id AS entrez,
+    splicing._tx_id AS tx_id,
+    cds._cds_id AS cds_id,
     cds_name,
     exon_rank
-FROM txdb_entrez.cds
-LEFT OUTER JOIN txdb_entrez.splicing ON cds._cds_id = splicing._cds_id
+FROM txdb_entrez.splicing
+LEFT OUTER JOIN txdb_entrez.cds ON cds._cds_id = splicing._cds_id
 LEFT OUTER JOIN txdb_entrez.transcript ON splicing._tx_id = transcript._tx_id
-LEFT OUTER JOIN txdb_entrez.gene ON transcript._tx_id = gene._tx_id;
+LEFT OUTER JOIN txdb_entrez.gene ON transcript._tx_id = gene._tx_id
+WHERE splicing._tx_id NOT IN 
+    (SELECT _tx_id FROM (SELECT distinct _tx_id, _cds_id from splicing)  
+         GROUP BY _tx_id HAVING count(*) = 1 AND _cds_id is NULL);
 
 CREATE INDEX IF NOT EXISTS entrez_ranges_cds on ranges_cds (entrez); 
 
