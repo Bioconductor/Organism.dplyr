7e6ab02b1dfebecdbf7edf95779842b8bde8b15d
Martin Morgan <martin.morgan@roswellpark.org>
2017-01-05 12:04:08 -0500

    minor cleanup to filter.R

--- R/filter.R
+++ R/filter.R
@@ -1,19 +1,19 @@
 #' Functions for filter objects
-#' 
-#' These functions are used to create filters for genomic extrators. 
-#' 
-#' All these filters except \code{GRangesFilter()} extend \code{BasicFilter} 
+#'
+#' These functions are used to create filters for genomic extrators.
+#'
+#' All these filters except \code{GRangesFilter()} extend \code{BasicFilter}
 #' class. Each filter takes value(s) from the corresponding column. For example,
-#' \code{AccnumFilter()} takes value of accession number(s), which come from 
-#' column \code{accnum}. 
-#' 
-#' \code{GRangesFilter()} takes a \code{GRanges} object as filter, and returns 
-#' genomic extractors (\code{genes}, \code{transcripts}, etc.) that are 
-#' partially overlapping with the region. 
-#' 
-#' \code{possibleFilters()} lists all available filters for \code{src_organism} 
+#' \code{AccnumFilter()} takes value of accession number(s), which come from
+#' column \code{accnum}.
+#'
+#' \code{GRangesFilter()} takes a \code{GRanges} object as filter, and returns
+#' genomic extractors (\code{genes}, \code{transcripts}, etc.) that are
+#' partially overlapping with the region.
+#'
+#' \code{possibleFilters()} lists all available filters for \code{src_organism}
 #' object.
-#' 
+#'
 #' @aliases AccnumFilter AliasFilter Cds_chromFilter Cds_idFilter Cds_nameFilter
 #'     Cds_strandFilter EnsemblFilter EnsemblprotFilter EnsembltransFilter
 #'     EntrezFilter EnzymeFilter EvidenceFilter EvidenceallFilter
@@ -25,7 +25,7 @@
 #'     Tx_typeFilter UnigeneFilter UniprotFilter Cds_startFilter Cds_endFilter
 #'     Exon_startFilter Exon_endFilter Gene_startFilter Gene_endFilter
 #'     Tx_startFilter Tx_endFilter
-#' 
+#'
 #' @usage AccnumFilter(value, condition = "==")
 #' AliasFilter(value, condition = "==")
 #' Cds_chromFilter(value, condition = "==")
@@ -74,40 +74,40 @@
 #' Gene_endFilter(value, condition = "==")
 #' Tx_startFilter(value, condition = "==")
 #' Tx_endFilter(value, condition = "==")
-#' 
-#' @param value Value of the filter. For \code{GRangesFilter} vaule should be a 
+#'
+#' @param value Value of the filter. For \code{GRangesFilter} vaule should be a
 #'     \code{GRanges} object.
-#' 
-#' @param condition The condition to be used in filter for genomic extractors, 
-#'     one of "==", "!=", "startsWith", "endsWith", ">", "<", ">=", "<=".  For 
+#'
+#' @param condition The condition to be used in filter for genomic extractors,
+#'     one of "==", "!=", "startsWith", "endsWith", ">", "<", ">=", "<=".  For
 #'     character values "==", "!=", "startsWith" and "endsWith" are allowed, for
-#'     numeric values (\code{Cds_startFilter}, \code{Cds_endFilter}, 
+#'     numeric values (\code{Cds_startFilter}, \code{Cds_endFilter},
 #'     \code{Exon_startFilter}, \code{Exon_endFilter}, \code{Gene_startFilter},
-#'     \code{Gene_endFilter}, \code{Tx_startFilter} and \code{Tx_endFilte}), 
+#'     \code{Gene_endFilter}, \code{Tx_startFilter} and \code{Tx_endFilte}),
 #'     "==", "!=", ">", ">=", "<" and "<=". Default condition is "==".
-#' 
-#' @seealso \code{\link{src_organism}} for creating a \code{src_organism} 
+#'
+#' @seealso \code{\link{src_organism}} for creating a \code{src_organism}
 #'     object.
-#'     
+#'
 #'     \code{\link[Organism.dplyr]{transcripts_tbl}} for generic functions
 #'      to extract genomic features from a \code{src_organism} object.
-#'      
-#'      \code{\link[Organism.dplyr]{select,src_organism-method}} for "select" 
-#'     interface on \code{src_organism} objects. 
-#' 
-#' @examples 
-#' ## filter by ensembl 
+#'
+#'      \code{\link[Organism.dplyr]{select,src_organism-method}} for "select"
+#'     interface on \code{src_organism} objects.
+#'
+#' @examples
+#' ## filter by ensembl
 #' EnsemblFilter("ENSG00000171862")
-#' 
+#'
 #' ## filter by gene symbol start with "BRAC"
 #' SymbolFilter("BRCA", "startsWith")
-#' 
+#'
 #' ## filter by GRanges
 #' GRangesFilter(as("chr10:87869000-87876000", "GRanges"))
-#' 
+#'
 #' ## filter by transcript start position
 #' Tx_startFilter(87863438,">")
-#' 
+#'
 #' @export AccnumFilter AliasFilter Cds_chromFilter Cds_idFilter Cds_nameFilter
 #' @export Cds_strandFilter EnsemblFilter EnsemblprotFilter EnsembltransFilter
 #' @export EntrezFilter EnzymeFilter EvidenceFilter EvidenceallFilter
@@ -127,7 +127,7 @@ setClass("BasicFilter",
              "VIRTUAL",
              field="character",
              condition="character",
-             value="character",
+             value="ANY",
              .valueIsCharacter="logical"
          ),
          prototype=list(
@@ -137,18 +137,17 @@ setClass("BasicFilter",
          )
 )
 
-
 #' @rdname BasicFilter
 #' @export
-setClass("GRangesFilter", 
+setClass("GRangesFilter",
          slots = list(field="character",
                       value="GRanges"))
 
 #' @rdname BasicFilter
 #' @export
 GRangesFilter <- function(value){
-    new("GRangesFilter", 
-        field="granges", 
+    new("GRangesFilter",
+        field="granges",
         value=value)
 }
 
@@ -171,21 +170,21 @@ setValidity("BasicFilter", function(object) {
         txt <- c(txt,
                  sprintf("'condition' must be one of %s",
                          paste("'", .OPS, "'", collapse=", ")))
-    if (isCharacter && !is.character(value)) 
-        txt <- c(txt, 
+    if (isCharacter && !is.character(value))
+        txt <- c(txt,
                  paste0("'", class(object),
                         "' can only take character value"))
-    if (!isCharacter && (!is.integer(value)) || is.na(value)) 
-        txt <- c(txt, 
+    if (!isCharacter && (!is.integer(value)) || is.na(value))
+        txt <- c(txt,
                  paste0("'", class(object),
                         "' can only take integer value"))
-    if (condition  %in% c("startsWith", "endsWith", ">", "<", ">=", "<=") && 
+    if (condition  %in% c("startsWith", "endsWith", ">", "<", ">=", "<=") &&
         length(value) > 1L)
-        txt <- c(txt, 
-                 paste0("'value' must be length 1 when condition is '", 
+        txt <- c(txt,
+                 paste0("'value' must be length 1 when condition is '",
                         condition, "'"))
     if (condition  %in% c(">", "<", ">=", "<=") && isCharacter)
-        txt <- c(txt, 
+        txt <- c(txt,
                  paste0("'", condition,
                         "' can only work with integer value"))
     if (length(txt)) txt else TRUE
@@ -194,27 +193,36 @@ setValidity("BasicFilter", function(object) {
 .OPS <- c("==", "!=", "startsWith", "endsWith", ">", "<", ">=", "<=")
 
 .CHAR_FIELDS <- c(
-    "accnum", "alias", "cds_chrom", "cds_id", "cds_name", 
-    "cds_strand", "ensembl", "ensemblprot", "ensembltrans", 
-    "entrez", "enzyme", "evidence", "evidenceall", "exon_chrom", 
-    "exon_id", "exon_name", "exon_rank", "exon_strand", 
-    "gene_chrom", "gene_strand", "genename", "go", "goall", 
-    "ipi", "map", "omim", "ontology", "ontologyall", "pfam", 
-    "pmid", "prosite", "refseq", "symbol", "tx_chrom", "tx_id", 
+    "accnum", "alias", "cds_chrom", "cds_id", "cds_name",
+    "cds_strand", "ensembl", "ensemblprot", "ensembltrans",
+    "entrez", "enzyme", "evidence", "evidenceall", "exon_chrom",
+    "exon_id", "exon_name", "exon_rank", "exon_strand",
+    "gene_chrom", "gene_strand", "genename", "go", "goall",
+    "ipi", "map", "omim", "ontology", "ontologyall", "pfam",
+    "pmid", "prosite", "refseq", "symbol", "tx_chrom", "tx_id",
     "tx_name", "tx_strand", "tx_type", "unigene", "uniprot")
 
 .INT_FIELDS <- c(
-    "cds_start", "cds_end", "exon_start", "exon_end", 
+    "cds_start", "cds_end", "exon_start", "exon_end",
     "gene_start", "gene_end", "tx_start", "tx_end")
 
 .filterFactory <- function(field, isCharacter) {
     class <- paste0(sub("([a-z])", "\\U\\1", field, perl=TRUE), "Filter")
     .valueIsCharacter <- isCharacter
+    .as.value <-
+        if (.valueIsCharacter) {
+            as.character
+        } else {
+            function(x) {
+                stopifnot(is.numeric(x))
+                as.integer(x)
+            }
+        }
     function(value, condition="==") {
         new(class,
             field=field,
             condition=condition,
-            value=as.character(value),
+            value=.as.value(value),
             .valueIsCharacter=.valueIsCharacter)
     }
 }
@@ -234,22 +242,17 @@ local({
 
 .condition <- function(x) x@condition
 
-.value <- function(x) {
-    if (is(x, "BasicFilter") && !.isCharacter(x))
-        as.integer(x@value)
-    else
-        x@value
-}
+.value <- function(x) x@value
 
 .isCharacter <- function(x) x@.valueIsCharacter
 
 
 #' @param object A \code{src_organism} object
-#' 
+#'
 #' @rdname BasicFilter
 #' @export
 setMethod("show", "BasicFilter", function(object){
-    cat("Object of class:", class(object), "\n")
+    cat("class:", class(object), "\n")
     cat("condition:", .condition(object), "\n")
     cat("value:", .value(object), "\n")
 })
@@ -262,18 +265,18 @@ setMethod("show", "BasicFilter", function(object){
     field <- .field(filter)
     value <- .value(filter)
     condition <- .condition(filter)
-    
+
     op <- switch(
         condition,
-        "==" = if (length(value) == 1) "==" else "%in%", 
+        "==" = if (length(value) == 1) "==" else "%in%",
         "!=" = if (length(value) == 1) "!=" else "%in%",
         "startsWith" = "%like%",
         "endsWith" = "%like%"
     )
-    
+
     if (condition %in% c("==", "!="))
         value <- paste0("'", value, "'", collapse=", ")
-    
+
     if (!is.null(op) && op %in% c("==", "!="))
         sprintf("%s %s %s", field, op, value)
     else if ((condition == "==") && op == "%in%")
@@ -281,9 +284,9 @@ setMethod("show", "BasicFilter", function(object){
     else if ((condition == "!=") && op == "%in%")
         sprintf("!%s %s c(%s)", field, op, value)
     else if (condition == "startsWith")
-        sprintf("%s %s %s", field, op, paste0("'", value, "%'"))
+        sprintf("%s %s '%s%%'", field, op, value)
     else if (condition == "endsWith")
-        sprintf("%s %s %s", field, op, paste0("'%", value, "'"))
+        sprintf("%s %s '%%%s'", field, op, value)
     else if (condition %in% c(">", "<", ">=", "<=")) {
         sprintf("%s %s %s", field, condition, as.integer(value))
     }
