1ad7c3ef708418d34c2e4241e62b58072c401847
Martin Morgan <martin.morgan@roswellpark.org>
2017-02-09 10:16:29 -0500

    add hg38light(), mm10light() helper functions

---
+++ R/utils.R
@@ -0,0 +1,26 @@
+#' Utilities used in examples, vignettes, and tests
+#'
+#' These functions are primarily for illustrating
+#' funtionality. \code{hg38light()} and \code{mm10light()} provide
+#' access to trimmed-down versions of Organism.dplyr data based
+#' derived from the TxDb.Hsapiens.UCSC.hg38.knownGene and
+#' TxDb.Mmusculus.UCSC.mm10.ensGene data bases.
+#'
+#' @return character(1) file path to the trimmed-down data base
+#' @examples
+#' hg38light()
+#' mm10light()
+#' @rdname utils
+#' @export
+hg38light <- function() {
+    system.file(
+        package="Organism.dplyr", "extdata", "light.hg38.knownGene.sqlite"
+    )
+}
+
+#' @export
+mm10light <- function() {
+    system.file(
+        package="Organism.dplyr", "extdata", "light.mm10.ensGene.sqlite"
+    )
+}
